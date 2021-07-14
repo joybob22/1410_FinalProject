72d3d2062a7d631e8bbcb5a955497e5eea815b5e
joybob22 <braydenlemke8@gmail.com>
2021-07-13 18:42:30 -0600

    created the starter project

---
+++ src/pongGame/GameFrame.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package pongGame;
+
+/**
+ * @author Not Carl Hulbert
+ *
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	GamePanel panel;
+	
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
---
+++ src/pongGame/PongGame.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package pongGame;
+
+/**
+ * @author Not Carl Hulbert
+ * 
+ * All source code for this project
+ * gotten from: 
+ * https://www.youtube.com/watch?v=oLirZqJFKPE
+ * 
+ *
+ */
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+		
+	}
+}
